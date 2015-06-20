ffd1aadd65eb674a494ec52c87df16b9b7a0c35c
Anshu <bajpai.anshumaan@gmail.com>
2015-06-20 16:32:27 -0400

    DevDataProd_Project

--- server.R
+++ server.R
@@ -5,14 +5,20 @@ mpgoutput <- function(cylinders = 6, weight = 3.0,
                       displacement = 160, horsepower = 110) {
         # Linear discriminant Analysis model
         data(mtcars)
+
+	# Fit a linear model for mileage as a function of 4 variables
         lm.fit <- lm(mpg ~ cyl + disp + wt + hp, data = mtcars)
         outputmpg <- predict(lm.fit, newdata = data.frame(cyl = cylinders, 
-                                                          disp = displacement, wt = weight,
-                                                          hp = horsepower), interval = "prediction")
+                                                          disp = displacement,
+							  wt = weight,
+                                                          hp = horsepower),
+							  interval = "prediction")
         rownames(outputmpg) <- "miles/gallon"
         colnames(outputmpg) <- c("Prediction", "Min", "Max")
         return(outputmpg)
 }
+
+
 shinyServer(
         function(input, output) {
                 output$Pairs <- renderPlot({pairs(~ mpg + disp + wt + hp + cyl, mtcars)})
